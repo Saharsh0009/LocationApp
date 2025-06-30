e50b821f5fa3cb9e207e97dbdf741c0ff2b5120f
Nikunj-Iguru <nikunj@iguru-india.com>
2025-06-30 18:39:24 +0530

    Changes

---
+++ app/src/main/java/com/nd/locationapp/LocationUtils.kt
@@ -0,0 +1,19 @@
+package com.nd.locationapp
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import android.Manifest
+
+class LocationUtils (val context: Context){
+    fun hasLocationPermission( context: Context ): Boolean {
+
+        if(ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+            return true
+        } else{
+            return false
+        }
+    }
+}
